4eed32fd8afaf804712162ba6e4ef1b33fbd3faa
Melaniecrzx <melanie.crzx@outlook.fr>
2026-04-23 11:13:10 +0200

    feat: type OverviewPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Main from "./layout/Main";
 
-const OverviewPage = lazy(() => import("./pages/OverviewPage"));
+const OverviewPage = lazy(() => import("./pages/OverviewPage.tsx"));
 const TransactionPage = lazy(() => import("./pages/TransactionPage.tsx"));
 const PortsPage = lazy(() => import("./pages/PotsPage"));
 const BudgetsPage = lazy(() => import("./pages/BudgetsPage.tsx"));
