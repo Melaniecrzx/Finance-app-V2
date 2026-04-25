73c29d8af8b79debada699b6d6a310d720a6d1d7
Melaniecrzx <melanie.crzx@outlook.fr>
2026-04-25 18:22:03 +0200

    fix : classname issue

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -27,7 +27,7 @@ export default function OverviewPage() {
 
         <div className="flex flex-col gap-6 lg:w-107.5 shrink-0">
           <BudgetsOverview />
-          <BillsOverview className="flex-1 h-full" />
+          <BillsOverview />
         </div>
       </div>
     </main>
